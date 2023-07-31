2463983de53d1725e2b5d80769204fb935aeec20
Pro100Paladin <pro100paladin@icloud.com>
2023-07-31 23:20:57 +0300

    решение задачи 27

--- Program.cs
+++ Program.cs
@@ -3,3 +3,18 @@
 //82 -> 10
 //9012 -> 12
 
+Console.WriteLine("Введите число");
+int i = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int summa = 0;
+while (i > 0)
+{
+    int number = i % 10;
+    i = i / 10;
+    summa = summa + number;
+    if (i < 1) {
+        i = 0;
+    }
+}
+Console.WriteLine("Сумма всех цифр числа: " + summa);
+
+    
